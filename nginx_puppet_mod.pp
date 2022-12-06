c0ea88cd81fc0c62513d82898d5a2306e9963c92
bfomnia <119955607+bfomnia@users.noreply.github.com>
2022-12-06 15:33:48 +0000

    Add files via upload

---
+++ nginx_puppet_mod.pp
@@ -0,0 +1,48 @@
+# Nginx webserver
+# Used documentation: https://forge.puppet.com/modules/puppet/nginx/readme
+# Used default nginx logging which should suffice for the requirement
+
+
+nginx::resource::server { 'puppet_netcentric_test':
+  members => {
+    'https://domain.com' => {
+      server => '10.10.10.10',
+      www_root => '/var/www/puppet_netcentric_test',
+      listen_port   => 443,
+      use_ssl => true,
+      ssl_cert => '/var/lib/puppet/ssl/certs/domain.com.pem',
+      ssl_key  => '/var/lib/puppet/ssl/private_keys/domain.com.pem',
+      access_log => '/var/log/nginx/domain.com_access.log',
+      error_log  => '/var/log/nginx/domain.com_error.log',
+      weight => 1,
+    },
+    'https://domain.com/resoure2' => {
+      server => '20.20.20.20',
+      www_root => '/var/www/resoure2',
+      listen_port   => 443,
+      use_ssl => true,
+      ssl_cert => '/var/lib/puppet/ssl/certs/domain.com.pem',
+      ssl_key  => '/var/lib/puppet/ssl/private_keys/domain.com.pem',
+      access_log => '/var/log/nginx/domain.com_access.log',
+      error_log  => '/var/log/nginx/domain.com_error.log',
+      weight => 1,
+    },
+}
+
+nginx::resource::server { 'rack.puppetlabs.com':
+  proxy => 'http://puppet_rack_app',
+}
+
+
+# Health Check
+# Used documentation: https://forge.puppet.com/modules/puppet/healthcheck/readme
+
+netcentric_test_cluster_nodes = ['https://domain.com', 'https://domain.com/resoure2']
+
+tcp_conn_validator { '$netcentric_test_cluster_nodes' :
+  listen_port   => 443,
+  use_ssl => true,
+  try_sleep => 10,
+  timeout => 50,
+}
+
